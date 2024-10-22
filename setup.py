6a46df3e91f17edb00c4ce6abc2a6b94c8ad5429
HSHMENG <141244101+hshmeng@users.noreply.github.com>
2024-10-22 18:44:07 +0800

    2024年10月22日18:44:05

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="hshmeng",
-    version="2024.10.22.4",
+    version="2024.10.22.5",
     author="hshmeng",
     author_email="hshmeng@foxmail.com",
     description='猛写的汉化',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    packages=find_packages(where="hshmeng_jet"),  # 指定源代码文件夹
+    packages=find_packages(),  # 自动查找所有包
     python_requires=">=3.8",
     license="MIT",
 )
