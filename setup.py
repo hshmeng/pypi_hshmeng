2ca04651814a811fb6140e713ca2302029124f26
HSHMENG <141244101+hshmeng@users.noreply.github.com>
2025-05-29 11:12:28 +0800

    2025年5月29日11:12:26

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hshmeng",
-    version="2025.5.29",
+    version="2025.5.29.1",
     author="hshmeng",
     author_email="hshmeng@foxmail.com",
     description='You hshmeng ZhiZuoDeZhongWenBianChengBao\n入门输入：from hshmeng.help import *\n帮助()',
